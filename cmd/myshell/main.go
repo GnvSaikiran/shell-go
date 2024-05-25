9bb4e1e1c97f917101e693159f00c6426cdf13d3
gnv <gajjarapusai1@gmail.com>
2024-05-25 20:14:48 +0530

    pass 3rd stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	reader := bufio.NewReader(os.Stdin)
 
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Print(err)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+
+		cmd, err := reader.ReadString('\n')
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				os.Exit(1)
+			}
+			fmt.Println(err)
+		}
+
+		fmt.Printf("%s: command not found\n", strings.TrimSpace(cmd))
 	}
-
-	fmt.Printf("%s: command not found\n", strings.TrimSpace(command))
 }
